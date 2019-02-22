d835e4e1459b52e73ebca6bfbd3302791cb3544b
Rob Purcell <rwgpurcell@gmail.com>
2019-02-22 16:01:41 -0500

    implement button set; some repeated observeEvent code remains

--- app.R
+++ app.R
@@ -1,27 +1,62 @@
 library(shiny)
 
 ui <- fluidPage(
-  actionButton("button1", "1"),br(),
-  actionButton("button2", "2"), br(),
-  hr(),
-  plotOutput("plot")
+  fluidRow(
+    column(2,
+      actionButton("button1", "1"),br(),
+      actionButton("button2", "2"),br(),
+      actionButton("button3", "3"),br(),
+      actionButton("button4", "4"),br(),
+      actionButton("button5", "5")
+    ),
+    column(10,
+      hr(),
+      textOutput("buttonValue"),
+      plotOutput("plot")
+    )
+  )
 )
 
 server <- function(input, output){
+
+  buttonData <- reactiveValues(presses=0)
+  
+  output$buttonValue <- renderText(buttonData$lastPress)
+  
+  observeEvent(input$button1, {
+    buttonData$presses <- buttonData$presses +1
+    buttonData$lastPress <- 1
+  })
+  
+  observeEvent(input$button2, {
+    buttonData$presses <- buttonData$presses +1
+    buttonData$lastPress <- 2
+  })
+  
+  observeEvent(input$button3, {
+    buttonData$presses <- buttonData$presses +1
+    buttonData$lastPress <- 3
+  })
+  
+  observeEvent(input$button4, {
+    buttonData$presses <- buttonData$presses +1
+    buttonData$lastPress <- 4
+  })
+  
+  observeEvent(input$button5, {
+    buttonData$presses <- buttonData$presses +1
+    buttonData$lastPress <- 5
+  })
+  
+  buttonSetAction <- observeEvent(c(buttonData$lastPress,buttonData$presses),{
+    v$data <- rnorm(10)
+  }) 
+  
   v <- reactiveValues(data = NULL)
   
-  observeEvent(input$runif, {
-    v$data <- runif(100)
-  })
-  
-  
-  observeEvent(input$runif, {
-    v$data <- runif(100)
-  })
-  
-  observeEvent(input$rnorm, {
-    v$data <- rnorm(100)
-  })  
+  # observeEvent(input$button2, {
+  #   v$data <- rnorm(100)
+  # })  
   
   output$plot <- renderPlot({
     if (is.null(v$data)) return()
