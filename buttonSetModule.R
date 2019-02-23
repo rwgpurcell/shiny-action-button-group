500ab0ce2a6c63d9f4096a95daf8d388fc5dd104
Rob Purcell <rwgpurcell@gmail.com>
2019-02-22 19:29:15 -0500

    clean up old code

--- buttonSetModule.R
+++ buttonSetModule.R
@@ -32,40 +32,4 @@ buttonSetModule <- function(input, output, session, choices, inline=FALSE,n_butt
   })
   
   return(buttonData)
-  
-  # # collect one year of data
-  # ydata <- reactive({
-  #   filter(data, year == input$year)
-  # })
-  # 
-  # xrange <- range(data$gdpPercap)
-  # yrange <- range(data$lifeExp)
-  # 
-  # output$plot <- renderPlot({
-  #   
-  #   # draw background plot with legend
-  #   plot(data$gdpPercap, data$lifeExp, type = "n", 
-  #        xlab = "GDP per capita", ylab = "Life Expectancy", 
-  #        panel.first = {
-  #          grid()
-  #          text(mean(xrange), mean(yrange), input$year, 
-  #               col = "grey90", cex = 5)
-  #        })
-  #   
-  #   legend("bottomright", legend = levels(data$continent), 
-  #          cex = 1.3, inset = 0.01, text.width = diff(xrange)/5,
-  #          fill = c("#E41A1C99", "#377EB899", "#4DAF4A99", 
-  #                   "#984EA399", "#FF7F0099"))
-  #   
-  #   # Determine bubble colors
-  #   cols <- c("Africa" = "#E41A1C99",
-  #             "Americas" = "#377EB899",
-  #             "Asia" = "#4DAF4A99",
-  #             "Europe" = "#984EA399",
-  #             "Oceania" = "#FF7F0099")[ydata()$continent]
-  #   
-  #   # add bubbles
-  #   symbols(ydata()$gdpPercap, ydata()$lifeExp, circles = sqrt(ydata()$pop),
-  #           bg = cols, inches = 0.5, fg = "white", add = TRUE)
-  # })
 }
